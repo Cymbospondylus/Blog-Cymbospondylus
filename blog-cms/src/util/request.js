5191c2ae434a3628e51d252c7175fd877ced7929
Cymbospondylus <2810560593@qq.com>
2022-10-26 19:20:05 +0800

    deploy

--- blog-cms/src/util/request.js
+++ blog-cms/src/util/request.js
@@ -4,7 +4,7 @@ import 'nprogress/nprogress.css'
 import {Message} from 'element-ui'
 
 const request = axios.create({
-	baseURL: 'http://localhost:8090/api/admin/',
+	baseURL: 'https://www.bzyl.site/api/admin/',
 	timeout: 5000
 })
 
