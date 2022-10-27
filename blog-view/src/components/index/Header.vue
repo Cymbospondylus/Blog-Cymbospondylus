d2f59d53a0e4b33fe9bf5a61bbab2e94b7c1843e
Cymbospondylus <2810560593@qq.com>
2022-10-27 23:43:02 +0800

    local image

--- blog-view/src/components/index/Header.vue
+++ blog-view/src/components/index/Header.vue
@@ -1,10 +1,10 @@
 <template>
 	<header ref="header">
 		<div class="view">
-			<img ref="imgbg1" src="https://fastly.jsdelivr.net/gh/Naccl/blog-resource/img/bg1.jpg" style="display: none;">
-			<div class="bg1" style="background-image: url('https://fastly.jsdelivr.net/gh/Naccl/blog-resource/img/bg1.jpg');"></div>
-			<div class="bg2" style="background-image: url('https://fastly.jsdelivr.net/gh/Naccl/blog-resource/img/bg2.jpg');"></div>
-			<div class="bg3" style="background-image: url('https://fastly.jsdelivr.net/gh/Naccl/blog-resource/img/bg3.jpg');" v-show="loaded"></div>
+			<img ref="imgbg1" src="https://www.bzyl.site/static/bg4.jpg" style="display: none;">
+			<div class="bg1" style="background-image: url('https://www.bzyl.site/static/bg1.jpg');"></div>
+			<div class="bg2" style="background-image: url('https://www.bzyl.site/static/bg2.jpg');"></div>
+			<div class="bg3" style="background-image: url('https://www.bzyl.site/static/bg3.jpg');" v-show="loaded"></div>
 		</div>
 		<div class="text-malfunction" data-word="Naccl's Blog">
 			<div class="line"></div>
